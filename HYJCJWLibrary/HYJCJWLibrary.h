503b87526fd2578466b48aa9f0552682a9bbcad8
WenFeng Zhang <wenfeng0218@icloud.com>
2015-09-07 15:44:14 +0800

    Create a new Xcode project (iOS) using Objective-C

---
+++ HYJCJWLibrary/HYJCJWLibrary.h
@@ -0,0 +1,13 @@
+//
+//  HYJCJWLibrary.h
+//  HYJCJWLibrary
+//
+//  Created by 张文峰 on 15/9/7.
+//  Copyright © 2015年 皓月君. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYJCJWLibrary : NSObject
+
+@end
---
+++ HYJCJWLibrary/HYJCJWLibrary.m
@@ -0,0 +1,15 @@
+//
+//  HYJCJWLibrary.m
+//  HYJCJWLibrary
+//
+//  Created by 张文峰 on 15/9/7.
+//  Copyright © 2015年 皓月君. All rights reserved.
+//
+
+#import "HYJCJWLibrary.h"
+
+@implementation HYJCJWLibrary
+- (void)sha1 {
+    return;
+}
+@end
